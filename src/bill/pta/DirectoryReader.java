2ca343d0f8b9770f2e5d0a9127b2b2ac293c3582
Bill Donges <bdonges@whatcounts.com>
2013-10-19 16:44:57 -0400

    file divider instead of hardcoded

--- src/bill/pta/DirectoryReader.java
+++ src/bill/pta/DirectoryReader.java
@@ -26,7 +26,9 @@ public class DirectoryReader
 			System.out.println("started reading file at " + new java.util.Date());
 			System.out.println("Working Directory = " + System.getProperty("user.dir"));			
 			
-			String dir = System.getProperty("user.dir") + "\\files\\2014\\";
+			
+			String div = "/";
+			String dir = System.getProperty("user.dir") + div + "files" + div + "2014" + div;
 			String file = "orig_13_14_directory_combined_columns_version.xlsx";
 
 			//..
